1b7df186d56255b274fb7d8d4c9fa7358b1db2a7
BlackWerf1257 <135800974+BlackWerf1257@users.noreply.github.com>
2023-12-14 20:47:17 +0900

    12.14_Update

---
+++ 14th/14th.cpp
@@ -0,0 +1,189 @@
+﻿using namespace std;
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <fstream>
+
+int square(int x) { return x * x; }
+
+int main()
+{
+    //예외 처리
+    int n, sum, average;
+    while (true)
+    {
+        cout << "합을 입력하세요 : ";
+        cin >> sum;
+        cout << "인원수를 입력하세요 : ";
+        cin >> n;
+
+        try {
+            if (n <= 0) //오류 탐지
+                throw n; //catch(int x) 블록으로 점프
+            else
+                average = sum / n;
+        }
+        catch (int x) {
+            cout << "예외 발생 " << x << "으로 나눌 수 없음" << endl;
+            average = 0;
+            cout << endl;
+            continue;
+        }
+        cout << "평균 : " << average << endl << endl; //평균 출력
+    }
+
+    //파일의 연결
+    /*const char* firstFile = "D:\\23-2\\CPP\\test_write2.txt";
+    const char* secondFile = "C:\\Windows\\system.ini";
+    
+    fstream fout(firstFile, ios::out | ios::app); //쓰기 모드로 파일 열기
+        if (!fout) {
+            cout << firstFile << "열기 오류";
+            return 0;
+        }
+
+        fstream fin(secondFile, ios::in); //읽기 모드로 파일 열기
+        if (!fin) {
+            cout << secondFile << "열기 오류";
+            return 0;
+        }
+        int c;
+        while ((c = fin.get()) != EOF)  //파일 끝까지 문자 읽기
+            fout.put(c); // 읽은 문자 기록
+
+        fin.close(); //입력 파일 닫기
+        fout.close(); //출력 파일 닫기*/
+
+
+    //파일 출력 예제
+    /*char name[10], dept[20];
+    int sid;
+    //키보드로부터 읽기
+    cout << " 이름 : ";
+    cin >> name; //키보드에서 이름 입력 받음
+    cout << " 학번 : ";
+    cin >> sid; 
+    cout << " 학과 : ";
+    cin >> dept;
+
+    //파일 열기 및 출력 스트림 생성
+    ofstream fd("D:\\23-2\\CPP\\test.txt");
+    if (!fd) { //열기 실패 검사
+        cout << "파일을 열수 없습니다.";
+        return 0;
+    }
+    //파일 쓰기
+    fd << name << endl; //name 쓰기
+    fd << sid << endl << endl;
+    fd << dept << endl;
+    cout << "파일 쓰기 성공" << endl;
+    fd.close(); //파일 닫기 */
+
+
+    /* //조작자
+    cout << showbase;
+
+    //타이틀 출력
+    cout << setw(8) << "Number";
+    cout << setw(10) << "Octal";
+    cout << setw(10) << "Hexa" << endl;
+
+    //하나의 수를 10, 8, 16진수 형태로 한줄에 출력
+    for (int i = 0; i < 50; i += 5) {
+        cout << setw(8) << setfill('.') << dec << i; //10진수
+        cout << setw(10) << setfill('.') << oct << i; //8진수
+        cout << setw(10) << setfill('.') << hex << i << endl; //16진수 
+    } */
+
+
+
+    //포맷 플래그
+    /* cout << 30 << endl; //10진수 출력
+    cout.unsetf(ios::dec); //10진수 해제
+    cout.setf(ios::hex); //16진수로 설정
+    cout << 30 << endl;
+
+    cout.setf(ios::showbase); //16진수로 설정
+    cout << 30 << endl;
+
+    cout.setf(ios::uppercase); //16진수의 A~F는 대문자로 출력
+    cout << 30 << endl;
+
+    cout.setf(ios::dec | ios::showpoint); //10진수 표현과 동시에 소수점 이하 나머지는 0으로 출력
+
+    cout << 23.5 << endl;
+
+    cout.setf(ios::scientific); //실수를 과학산술용 표현으로 출력
+    cout << 23.5 << endl;
+
+    cout.setf(ios::showpos); //양수인 경우 + 부호도 함께 출력
+    cout << 23.5; */
+
+    //auto 이용한 변수 선언
+    /* auto c = 'a'; //char 타입
+    auto pi = 3.14; //double 타입
+    auto one = 1; //int
+    auto* p = &one; //int* 타입
+
+    cout << c << " " << pi << " " << one << " " << *p << endl;
+
+    //함수의 리턴 타입으로 추론
+    auto ret = square(3); //square()의 리턴타입이 int이므로 int
+
+    cout << *p << " " << ret << endl;
+
+    vector<int> vec = { 1,2,3,4,5 }; //벡터 vec에 5개 원소 1,2,3,4,5 삽입
+    vector<int>::iterator iter;
+    for (iter = vec.begin(); iter != vec.end(); iter++)
+        cout << *iter << " "; //1 2 3 4 5 출력
+    cout << endl;
+
+    //템플릿에 auto를 사용해 간소화
+    for (auto iter = vec.begin(); iter != vec.end(); iter++)
+        cout << *iter << " "; //1 2 3 4 5 출력 */
+
+
+    //STL 알고리즘 sort()의 예제
+    /*vector<int> v; //정수 벡터 생성
+    cout << "5개의 정수 입력하세요 : ";
+        for (int i = 0; i < 5; i++)
+        {
+            int n;
+            cin >> n;
+            v.push_back(n); //키보드에 읽은 정수를 벡터에 삽입
+        }
+        //v.begin() - v.end() 사이의 값을 오름차순으로 정렬
+        //sort() 함수의 실행결과 : 벡터 v의 원소 순서 변경
+        sort(v.begin(), v.end());
+        
+        vector<int>::iterator it; //벡터 내 원소를 탐색하는 iterator 변수 선언
+        for (it = v.begin(); it != v.end(); it++)
+            cout << *it << ' ';
+        cout << endl;*/
+
+    //Map의 활용
+    /*map<string, string> dic;
+    dic.insert(make_pair("love", "사랑"));
+    dic.insert(make_pair("apple", "사과"));
+
+    dic["cherrry"] = "체리"; //("cherry", "체리") 저장
+    cout << "사전의 단어 수 : " << dic.size() << endl;
+
+    string eng;
+    while (true)
+    {
+        cout << "찾고 싶은 단어 :";
+        getline(cin, eng); //사용자로부터 단어 입력
+
+        if (eng == "exit")
+            break;
+        if (dic.find(eng) == dic.end()) //해당 키가 존재 X할 경우
+            cout << "값 존재 X" << endl;
+        else
+            cout << dic[eng] << endl;
+    }
+    cout << "종료" << endl; */
+}
---
+++ 5th/CPP5.cpp
@@ -0,0 +1,264 @@
+﻿#include <iostream>
+using namespace std;
+//#include "Circle.h"
+// 
+//동적 할당 응용 3
+class DynamicCircleEx3 {
+    int radius;
+public:
+    DynamicCircleEx3();
+    ~DynamicCircleEx3();
+    void setRadius(int r) { radius = r; }
+    double getArea() { return 3.14 * radius * radius; }
+};
+DynamicCircleEx3::DynamicCircleEx3()
+{
+    radius = 1;
+}
+
+
+
+
+
+//동적 할당 응용
+class DynamicCircleEx {
+    int radius;
+public:
+    DynamicCircleEx();
+    DynamicCircleEx(int r);
+    ~DynamicCircleEx();
+    void setRadius(int r) { radius = r; }
+    double getArea() { return 3.14 * radius * radius; }
+};
+DynamicCircleEx::DynamicCircleEx()
+{
+    radius = 1;
+    cout << "생성자 실행 radius = " << radius << endl;
+}
+DynamicCircleEx::DynamicCircleEx(int r)
+{
+    radius = r;
+    cout << "생성자 실행 radius = " << radius << endl;
+}
+DynamicCircleEx::~DynamicCircleEx() {
+    cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+
+
+//동적 할당
+class DynamicCircle {
+    int radius;
+    public:
+        DynamicCircle();
+        DynamicCircle(int r);
+        ~DynamicCircle();
+        void setRadius(int r) { radius = r; }
+        double getArea() { return 3.14 * radius * radius; }
+};
+DynamicCircle::DynamicCircle()
+{
+    radius = 1;
+    cout << "생성자 실행 radius = " << radius << endl;
+}
+DynamicCircle::DynamicCircle(int r)
+{
+    radius = r;
+    cout << "생성자 실행 radius = " << radius << endl;
+}
+DynamicCircle::~DynamicCircle() {
+    cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+
+class CircleMultiArray {
+    int radius;
+public:
+    CircleMultiArray() { radius = 1; }
+    CircleMultiArray(int r) { radius = r; }
+    void setRadius(int r)
+    {
+        radius = r;
+    }
+    double getArea();
+};
+double CircleMultiArray::getArea() {
+    return 3.14 * radius * radius;
+}
+
+
+class CircleArray {
+    int radius;
+    public:
+        CircleArray() { radius = 1; }
+        CircleArray(int r) { radius = r; }
+        void setRadius(int r)
+            {radius = r; }
+        double getArea();
+};
+double CircleArray::getArea() {
+    return 3.14 * radius * radius;
+    }
+
+
+class CirclePointer {
+    int radius;
+    public:
+        CirclePointer() { radius = 1; }
+        CirclePointer(int r) { radius = r; }
+        double getArea();
+};
+double CirclePointer::getArea() {
+    return 3.14 * radius * radius;
+}
+
+int main()
+{
+    //객체의 동적 할당 응용 3
+    //cout << "생성하는 원의 개수 : ";
+    //int n, radius;
+    //cin >> n;
+    //DynamicCircleEx3 *pArrary = new DynamicCircleEx3[n];
+    //for (int i = 0; i < n; i++)
+    //{
+    //    cout << "원" << i + 1 << ":";
+    //    cin >> radius; //반지름 입력
+    //    pArrary[i].setRadius(radius);
+    //}
+    //int count = 0; //카운트 변수
+    //DynamicCircleEx3 *p = pArrary;
+    //for (int i = 0; i < n; i++)
+    //{
+    //    cout << p->getArea() << ' ';
+    //    if (p->getArea() >= 100 && p->getArea() <= 300)
+    //        count++;
+    //    p++;
+    //}
+
+    //cout << endl << "면적이 100 ~ 300 사이인 원의 개수 : " 
+    //    << count << endl;
+    //delete[] pArrary;
+
+
+    //객체의 동적 할당 응용
+    //int radius;
+    //while (true)
+    //{
+    //    cout << "정수 반지름 입력(음수면 종료) >>";
+    //    cin >> radius;
+    //    if (radius < 0) break; //음수가 입력되어 종료
+
+    //    DynamicCircleEx* p = new DynamicCircleEx(radius); //동적 객체 생성
+    //    cout << "원의 면적은 " << p->getArea() << endl;
+    //    delete p; //객체 반환
+    //}
+    
+
+
+    // //객체의 동적 할당
+    /*DynamicCircle *p, * q;
+    p = new DynamicCircle;
+    q = new DynamicCircle(30);
+    cout << p->getArea() << endl << q->getArea() << endl;
+    delete p;
+    delete q;*/
+
+
+
+    //동적 메모리 사용
+    //cout << "입력 개수는?";
+    //int n;
+    //cin >> n;
+    //if (n <= 0) return 0;
+    //int* p = new int[n];
+    //if (!p) {
+    //    cout << "메모리를 할당할 수 없습니다.";
+    //    return 0;
+    //}
+    //for (int i = 0; i < n; i++)
+    //{
+    //    cout << i + 1 << "번째 입력 : ";
+    //    cin >> p[i]; //정수 입력
+    //}
+    //int sum = 0;
+    //for (int i = 0; i < n; i++)
+    //    sum += p[i];
+    //cout << "총합 = " << sum << endl;
+    //delete[] p; //배열 메모리 반환
+
+
+
+    //동적 메모리 할당 연산자
+    int *p;
+    p = new int;
+    if (!p)
+    {
+        cout << "메모리를 할당할 수 없습니다";
+        return 0;
+    }
+    *p = 5;
+    int n = *p;
+    cout << "*p = " << *p << '\n';
+    cout << "n = " << n << '\n';
+    delete p;
+
+
+    //다차원 객체 배열
+    /*CircleMultiArray donutArray[2][3];
+    donutArray[0][0].setRadius(1);
+    donutArray[0][1].setRadius(2);
+    donutArray[0][2].setRadius(3);
+    donutArray[1][0].setRadius(4);
+    donutArray[1][1].setRadius(5);
+    donutArray[1][2].setRadius(6);
+
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++)
+            cout << "Donut[" << i << "," << j << "]의 면적은 " << donutArray[i][j].getArea() << endl;*/
+
+
+    //객체 배열
+    //CircleArray donutArray[3];
+    ////배열을 이용한 객체 접근
+    //donutArray[0].setRadius(10);
+    //donutArray[1].setRadius(20);
+    //donutArray[2].setRadius(30);
+
+    //for (int i = 0; i < 3; i++)
+    //    cout << "Donut " << i << " 의 면적은 " << donutArray[i].getArea() << endl;
+    //CircleArray* p;
+    //p = donutArray;
+    //for (int i = 0; i < 3; i++) {
+    //    cout << "Donut " << i << " 의 면적은" << p->getArea() << endl;
+    //    p++;
+    //}
+
+    //객체 포인터
+    //CirclePointer donut;
+    //CirclePointer pizza(30);
+
+    ////객체 이름으로 멤버 접근
+    //CirclePointer *p;
+    //p = &donut;
+    //cout << p->getArea() << endl; //donut의 getArea() 호출
+    //cout << (*p).getArea() << endl; //donut의 getArea() 호출
+
+    //p = &pizza;
+    //cout << p->getArea() << endl; //pizza의 getArea() 호출
+    //cout << (*p).getArea() << endl; //pizza의 getArea() 호출
+
+
+
+    //파일 분리
+    /*Circle donut;
+    double area = donut.getArea();
+    cout << " donut 면적은 :";
+    cout << area << endl;
+
+    Circle pizza(30);
+    area = pizza.getArea();
+    cout << "pizza 면적은: ";
+    cout << area << endl;*/
+}
+
+
