21e56ee8329a4948b6b58756e5780dac993f640d
BlackWerf1257 <135800974+BlackWerf1257@users.noreply.github.com>
2023-12-20 15:35:45 +0900

    파일 추가

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Test
+{
+
+
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        Static.b = a;
+        Static st = new Static();
+
+        Console.WriteLine(Static.b++);
+        Console.WriteLine(Static.b);
+        Console.WriteLine(a);
+        Console.WriteLine(st.a);
+    }
+
+    
+}
+
+class Static
+{
+    public int a = 20;
+    public static int b = 0;
+}
